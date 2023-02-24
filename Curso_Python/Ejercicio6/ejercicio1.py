ffcbe2e3fc3304425827f1de478baabb0d881aa8
Ciscode <vignardel.francisco@gmail.com>
2023-02-24 00:14:40 -0300

    ejercicio1

---
+++ Curso_Python/Ejercicio6/ejercicio1.py
@@ -0,0 +1,23 @@
+class Vehiculo:
+    #inicializar atributos
+    def __init__(self, color, ruedas, puertas):
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
+    
+    def __str__(self):
+        return "Color {}, {} ruedas".format( self.color, self.ruedas, self.puertas )
+    
+class Coche(Vehiculo):
+    def __init__(self, color, ruedas, puertas,velocidad,cilindrada):
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+        
+    def __str__(self):
+        return "color {}, {} km/h, {} ruedas, {} puertas, {} cc".format( self.color, self.velocidad, self.ruedas, self.puertas, self.cilindrada )
+
+coche = Coche("amarillo", 4, 2, 200, 1600)
+print(coche)
