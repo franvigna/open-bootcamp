fb1e807296902fe76ed6443541bb4f495118b89f
Ciscode <vignardel.francisco@gmail.com>
2023-02-28 17:31:52 -0300

    Ejercicio 2

--- Curso_Python/Ejercicio10/ejercicio2.py
+++ Curso_Python/Ejercicio10/ejercicio2.py
@@ -4,7 +4,7 @@ import tkinter
 window = Tk()
 
 # Set the size of the windowdow
-window.geometry("700x350")
+window.geometry("400x350")
 
 
 window.columnconfigure(0, weight=1)
@@ -14,9 +14,12 @@ window.columnconfigure(1, weight=5)
 
 lista = ['Python','C++','Java','PHP','JavaScript']
 lista_items = tkinter.StringVar(value=lista)
-listbox = tkinter.Listbox(window, height=20, listvariable=lista_items)
+listbox = tkinter.Listbox(window, height=10, listvariable=lista_items)
 
 listbox.grid(column=0,row=0,sticky=tkinter.W)
+listbox.pack()
 
+label = Label(text="¿Que curso queres aprender?")
+label.pack()
 
 window.mainloop()
